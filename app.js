e9c7bcb31263b6a384065bd8c97832259f40fada
Austin Summerlin <summerlin.austin@gmail.com>
2021-03-24 15:35:02 -0700

    add attempt counting

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const guessInput = document.getElementById('guess-input');
 const results = document.getElementById('results');
 const button = document.getElementById('guess-button');
 const attemptsNum = document.getElementById('attempts');
+const reset = document.getElementById('reset');
+const highestNumber = Number(Math.ceil(Math.random() * 10));
 
 let attempts = 5;
 
 button.addEventListener ('click', () => {
     attempts--;
     attemptsNum.textContent = attempts;
+
 });
+    // if (compareNumbers(Number(guessInput.value), highestNumber)) {};

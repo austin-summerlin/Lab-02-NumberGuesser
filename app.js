89e9883c0a3e72273c3e300b52d29b0fc41ee5de
Austin Summerlin <summerlin.austin@gmail.com>
2021-03-24 17:24:04 -0700

    beautified

--- app.js
+++ app.js
@@ -11,19 +11,21 @@ let attempts = 5;
 let specialNumber = Math.ceil(Math.random() * 10);
 
 button.addEventListener ('click', () => {
+    
     attempts--;
     attemptsNum.textContent = attempts;
     
     const userGuess = Number(guessInput.value);
     const result = compareNumbers(userGuess, specialNumber);
 
-    if(result === 0){
+    if (result === 0){
+        button.disabled = true;
         results.textContent = 'Good Job, you guessed correctly!';
     }
-    if(result === 1){
+    if (result === 1){
         results.textContent = 'Too high, try again.';
     }
-    if(result === -1){
+    if (result === -1){
         results.textContent = 'Too low, try again.';
     }
     if (attempts === 0){
@@ -33,6 +35,7 @@ button.addEventListener ('click', () => {
 });
 
 reset.addEventListener ('click', () => {
+
 });
 
  
